8b228f36e4d20a48d38affb1fe4552eef23cea80
Sheshmani Patel <sumitkumar47581@gmail.com>
2024-07-18 12:12:34 +0530

    Deleting unusual stuff

--- script.js
+++ script.js
@@ -14,18 +14,4 @@ if (close) {
     nav.classList.remove("active");
   });
 }
-
-
-// fullscreen
-function requestFullscreen() {
-  const elem = document.documentElement;
-  if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { // Firefox
-      elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-      elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { // Internet Explorer/Edge
-      elem.msRequestFullscreen();
-  }
-}
+ 
