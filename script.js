9b4c34b4fac2ae33c2057a00a3b89da5f306fa5f
Sheshmani Patel <sumitkumar47581@gmail.com>
2024-07-18 12:06:50 +0530

    last try for check notch device

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+
+// fullscreen
+function requestFullscreen() {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { // Firefox
+      elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+      elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // Internet Explorer/Edge
+      elem.msRequestFullscreen();
+  }
+}
