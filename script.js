c2bbc815469200f0837314cdecff56b3b4f8e6c8
Sheshmani Patel <sumitkumar47581@gmail.com>
2024-07-18 15:29:30 +0530

    Worked on product details page

--- script.js
+++ script.js
@@ -14,4 +14,21 @@ if (close) {
     nav.classList.remove("active");
   });
 }
- 
+
+// Select all elements with the class name 'product'
+const products = document.querySelectorAll(".product");
+
+// Loop through each element and add the onclick event
+products.forEach((product) => {
+  product.addEventListener("click", () => {
+    const imgElement = product.querySelector("img");
+    window.location.href = "sproduct.html";
+    if (imgElement) {
+      const imgSrc = imgElement.src;
+      // Encode the image source URL to pass as a parameter
+      const encodedImgSrc = encodeURIComponent(imgSrc);
+      // Redirect to sproduct.html with encodedImgSrc as parameter
+      window.location.href = `sproduct.html?img=${encodedImgSrc}`;
+    }
+  });
+});
